e4b10f7905495f74e14bd422e2860d4d782f7567
iSa1vatore <Skaldak1337@gmail.com>
2019-03-22 13:13:15 +0500

    Fixes

--- VKCallBackBot/VKCallBackBotConversation.php
+++ VKCallBackBot/VKCallBackBotConversation.php
@@ -4,39 +4,39 @@ class VKCallBackBotConversation
 {
     private $bot;
 
-	public function __construct($bot)
-	{
-		$this->bot = $bot;
-	}
+    public function __construct($bot)
+    {
+        $this->bot = $bot;
+    }
 
-	public function members()
-	{
-		return $this->bot->api->getConversationMembers([
-			'peer_id' => $this->bot->api->receiver
-		]);
-	}
+    public function members()
+    {
+        return $this->bot->api->getConversationMembers([
+            'peer_id' => $this->bot->api->receiver
+        ]);
+    }
 
-	public function administrators()
-	{
-		$data = $this->members();
+    public function administrators()
+    {
+        $data = $this->members();
 
         $admins = [];
 
-		foreach ($data['items'] as $member) {
-			if($member['is_admin']) {
-				$admins[] = $member['member_id'];
-			}
-		}
+        foreach ($data['items'] as $member) {
+            if ($member['is_admin']) {
+                $admins[] = $member['member_id'];
+            }
+        }
 
-		return $admins;
-	}
+        return $admins;
+    }
 
-	public function kick(int $uid)
-	{
-		return $this->bot->api->removeChatUser([
-			'chat_id' => $this->bot->api->receiver,
-			'user_id' => $uid,
-			'member_id' => $uid,
-		]);
-	}
+    public function kick(int $uid)
+    {
+        return $this->bot->api->removeChatUser([
+            'chat_id' => $this->bot->api->receiver,
+            'user_id' => $uid,
+            'member_id' => $uid,
+        ]);
+    }
 }
--- VKCallBackBot/VKCallBackBotEvents.php
+++ VKCallBackBot/VKCallBackBotEvents.php
@@ -2,21 +2,21 @@
 
 class VKCallBackBotEvents
 {
-	private $bot;
-	private $LongPollData;
+    private $bot;
+    private $LongPollData;
 
-	public function __construct($bot)
-	{
-		$this->bot = $bot;
-		$this->LongPollData = $this->bot->api->getLongPollServer();
-	}
+    public function __construct($bot)
+    {
+        $this->bot = $bot;
+        $this->LongPollData = $this->bot->api->getLongPollServer();
+    }
 
-	public function get()
-	{
-		$LongPollData = $this->bot->api->curlRequest($this->LongPollData['server'] . '?act=a_check&key=' . $this->LongPollData['key'] . '&ts=' . $this->LongPollData['ts'] . '&wait=30', []);
+    public function get()
+    {
+        $LongPollData = $this->bot->api->curlRequest($this->LongPollData['server'] . '?act=a_check&key=' . $this->LongPollData['key'] . '&ts=' . $this->LongPollData['ts'] . '&wait=30', []);
 
-		$this->LongPollData['ts'] = $LongPollData['ts'];
+        $this->LongPollData['ts'] = $LongPollData['ts'];
 
-		return $LongPollData['updates'];
-	}
+        return $LongPollData['updates'];
+    }
 }
